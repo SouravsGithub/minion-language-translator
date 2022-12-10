e4d37b4857abd5abab0cd39cb29c419ff2cedd8b
SouravsGithub <souravsemail2001@gmail.com>
2022-12-10 17:00:33 +0530

    did the exercise

--- main.js
+++ main.js
@@ -4,3 +4,9 @@ let textInput = document.querySelector("#text-input");
 function clickEventHandler() {
   console.log(textInput.value);
 }
+
+// Exercise time
+document.querySelector("#text-input");
+document.querySelector(".btn-primary");
+document.querySelector("#input-btn");
+document.querySelector("[name=translator]");
