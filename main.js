ff6f8eee45e7ad5ab09a220d367ea98c7c3b2945
SouravsGithub <souravsemail2001@gmail.com>
2022-12-11 12:06:47 +0530

    made the whole web app with a mock server

--- main.js
+++ main.js
@@ -2,7 +2,21 @@ const btn = document.querySelector("button");
 const textInput = document.querySelector("#text-input");
 const outputDiv = document.querySelector(".output");
 
-function clickEventHandler() {
-  console.log(textInput.value);
-  outputDiv.innerText = "ajajajajajaja " + textInput.value;
+btn.addEventListener("click", getTranslation);
+const url = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json";
+
+function urlConstructor(text) {
+  return url + "?" + "text=" + text;
+}
+function getTranslation() {
+  fetch(urlConstructor(textInput.value))
+    .then((response) => {
+      return response.json();
+    })
+    .then((json) => {
+      outputDiv.innerText = json.contents.translated;
+    })
+    .catch((error) => {
+      console.log("error occured: " + error);
+    });
 }
